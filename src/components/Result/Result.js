10f6557ac636ea34977f97eab5521e715dca10c6
=rithgan <=rithikgandhi1989@gmail.com>
2021-02-15 00:17:36 +0530

    toogle html and text

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import marked from "marked";
 import DOMpurify from "dompurify";
+import ShowHtml from "../ShowHtml";
 import styles from "./Result.module.css";
 
 const Result = (props) => {
   const input = props.children;
+  const html = DOMpurify.sanitize(marked(input));
+  const [visibility, setVisibility] = useState(false);
+  const handleVisibility = () => {
+    setVisibility(!visibility);
+  };
 
   return (
     <>
-      <article
-        className={styles.result}
-        dangerouslySetInnerHTML={{ __html: DOMpurify.sanitize(marked(input)) }}
-      ></article>
+      <div>
+        <button onClick={handleVisibility}>
+          {!visibility ? "</>" : "text"}
+        </button>
+        {!visibility ? (
+          <article
+            className={styles.result}
+            dangerouslySetInnerHTML={{
+              __html: html,
+            }}
+          ></article>
+        ) : null}
+
+        {visibility ? <ShowHtml input={html} /> : null}
+      </div>
     </>
   );
 };
+
 export default Result;
