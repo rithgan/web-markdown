2db5c6b812f018ebaf0921b037729124bedf16fd
=rithgan <=rithikgandhi1989@gmail.com>
2021-02-14 16:06:23 +0530

    text passing as children

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { useState } from "react";
 import "./App.css";
+import Result from "./components/Result/Result.js";
 
 function App() {
-  return <div></div>;
+  const [value, setValue] = useState("# markdown preview");
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+  return (
+    <div>
+      <textarea value={value} onChange={handleChange}></textarea>
+      <Result>{value}</Result>
+    </div>
+  );
 }
 
 export default App;
