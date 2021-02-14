5c1e44625fcadcefdeed42c7c514e8944080787b
=rithgan <=rithikgandhi1989@gmail.com>
2021-02-14 16:53:14 +0530

    dompurified

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,11 +1,15 @@
 import React from "react";
 import marked from "marked";
+import DOMpurify from "dompurify";
+
 const Result = (props) => {
   const input = props.children;
 
   return (
     <>
-      <div dangerouslySetInnerHTML={{ __html: marked(input) }}></div>
+      <div
+        dangerouslySetInnerHTML={{ __html: DOMpurify.sanitize(marked(input)) }}
+      ></div>
     </>
   );
 };
